5e3aaa9413282fd80c5c86827fc686864f396d81
tianchen <tczhong24@gmail.com>
2021-01-12 20:37:37 -0800

    make add new sub item easier

--- src/App.js
+++ src/App.js
@@ -1,101 +1,174 @@
-import logo from './logo.svg';
-import './App.css';
-import "antd/dist/antd.css"
-import LatestWeekly from './components/LatestWeekly';
-import LatestMonthly from './components/LatestMonthly';
-import EditPage from './components/EditPage';
-import MarkdownPage from './components/MarkdownPage';
-import ReadList from './components/ReadList';
-import ShowPage from './components/ShowPage';
-import { BrowserRouter as Router, Route, Link,useLocation } from "react-router-dom";
-import useState from 'react';
-import {  Menu,Layout } from 'antd';
-import {  BookOutlined, UserOutlined, AimOutlined,EditOutlined,FolderViewOutlined,HeartOutlined  } from '@ant-design/icons';
-const { Header, Sider, Content} = Layout;
+import logo from "./logo.svg";
+import "./App.css";
+import "antd/dist/antd.css";
+import LatestWeekly from "./components/LatestWeekly";
+import LatestMonthly from "./components/LatestMonthly";
+import EditPage from "./components/EditPage";
+import MarkdownPage from "./components/MarkdownPage";
+import ReadList from "./components/ReadList";
+import ShowPage from "./components/ShowPage";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  useLocation,
+} from "react-router-dom";
+import useState from "react";
+import { Menu, Layout } from "antd";
+import {
+  BookOutlined,
+  UserOutlined,
+  AimOutlined,
+  EditOutlined,
+  FolderViewOutlined,
+  HeartOutlined,
+  SafetyCertificateOutlined,
+} from "@ant-design/icons";
+const { Header, Sider, Content } = Layout;
 function useQuery() {
-    return new URLSearchParams(useLocation().search);
-  }
+  return new URLSearchParams(useLocation().search);
+}
 const { SubMenu } = Menu;
 function App(props) {
-    let query = useQuery();
+  const sub = [
+    { name: "每周计划", json: "weekly" },
+    { name: "每周工作总结", json: "weekly_work" },
+    { name: "每月计划", json: "monthly" },
+    { name: "项目", json: "project" },
+    { name: "朋友", json: "friends" },
+    { name: "睡前故事", json: "sleep_story" },
+    { name: "故事", json: "story" },
+    { name: "笑话", json: "fun" },
+  ];
+  let query = useQuery();
   return (
-  
     <Layout>
-    <Sider
-      breakpoint="lg"
-      collapsedWidth="0"
-      onBreakpoint={broken => {
-        console.log(broken);
-      }}
-      onCollapse={(collapsed, type) => {
-        console.log(collapsed, type);
-      }}
-    >
-      <div className="logo" />
-      <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-        <Menu.Item key="1" icon={<UserOutlined />}>
-          自己提高的点
-          <Link to="/improvement" />
-        </Menu.Item>
-        <Menu.Item key="10" icon={<UserOutlined />}>
-          原则
-          <Link to="/principe" />
-        </Menu.Item>
-        <Menu.Item key="11" icon={<HeartOutlined />}>
-          同理心
-          <Link to="/empthy" />
-        </Menu.Item>
-        <Menu.Item key="2" icon={<AimOutlined />}>
-          本周目标
-          <Link to="/weekly" />
-        </Menu.Item>
-        <Menu.Item key="3" icon={<AimOutlined />}>
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={(broken) => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+      >
+        <div className="logo" />
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+          <Menu.Item key="1" icon={<UserOutlined />}>
+            自己提高的点
+            <Link to="/improvement" />
+          </Menu.Item>
+          <Menu.Item key="10" icon={<UserOutlined />}>
+            原则
+            <Link to="/principe" />
+          </Menu.Item>
+          <Menu.Item key="11" icon={<HeartOutlined />}>
+            同理心
+            <Link to="/empthy" />
+          </Menu.Item>
+          <Menu.Item key="12" icon={<SafetyCertificateOutlined />}>
+            应急处理
+            <Link to="/safety" />
+          </Menu.Item>
+          <Menu.Item key="2" icon={<AimOutlined />}>
+            本周目标
+            <Link to="/weekly" />
+          </Menu.Item>
+          <Menu.Item key="3" icon={<AimOutlined />}>
             本月目标
             <Link to="/monthly" />
-        </Menu.Item>
-        <Menu.Item key="4" icon={<BookOutlined />}>
-          读书
-          <Link to="/read" />
-        </Menu.Item>
-        <SubMenu key="sub1" icon={<EditOutlined />} title="修改">
-            <Menu.Item key="weekly_edit">每周计划<Link to="/weekly_edit" /></Menu.Item>
-            <Menu.Item key="weekly_work_edit">每周工作总结<Link to="/weekly_work_edit" /></Menu.Item>
-            <Menu.Item key="monthly_edit">每月计划<Link to="/monthly_edit" /></Menu.Item>
-            <Menu.Item key="friends">朋友<Link to="/friends_edit" /></Menu.Item>
+          </Menu.Item>
+          <Menu.Item key="4" icon={<BookOutlined />}>
+            读书
+            <Link to="/read" />
+          </Menu.Item>
+          <SubMenu key="sub1" icon={<EditOutlined />} title="修改">
+            {sub.map((item) => {
+              const key = `${item.json}_edit`;
+              return (
+                <Menu.Item key={key}>
+                  {item.name}
+                  <Link to={key} />
+                </Menu.Item>
+              );
+            })}
           </SubMenu>
           <SubMenu key="sub2" icon={<FolderViewOutlined />} title="查看">
-            <Menu.Item key="weekly_view">每周计划<Link to="/weekly_view" /></Menu.Item>
-            <Menu.Item key="month_view">每月计划<Link to="/monthly_view" /></Menu.Item>
-            <Menu.Item key="friends_view">朋友<Link to="/friends_view" /></Menu.Item>
+            {sub.map((item) => {
+              const key = `${item.json}_view`;
+              return (
+                <Menu.Item key={key}>
+                  {item.name}
+                  <Link to={key} />
+                </Menu.Item>
+              );
+            })}
           </SubMenu>
-        
-      </Menu>
-    </Sider>
-    <Layout>
-      <Header className="site-layout-sub-header-background" style={{ padding: 0 }} />
-      <Content style={{ margin: '24px 16px 0' }}>
-        <div className="site-layout-background" style={{ padding: 24, minHeight: 1024 }}>
-        <Route exact path="/" component={Improvement} />
-            <Route path="/improvement" component={<MarkdownPage name="我能做的好的地方.md" list="must"/>} />
+        </Menu>
+      </Sider>
+      <Layout>
+        <Header
+          className="site-layout-sub-header-background"
+          style={{ padding: 0 }}
+        />
+        <Content style={{ margin: "24px 16px 0" }}>
+          <div
+            className="site-layout-background"
+            style={{ padding: 24, minHeight: 1024 }}
+          >
+            <Route
+              exact
+              path="/"
+              children={<MarkdownPage name="我能做的好的地方.md" list="must" />}
+            />
+            <Route
+              path="/improvement"
+              children={<MarkdownPage name="我能做的好的地方.md" list="must" />}
+            />
+            <Route
+              path="/safety"
+              children={<MarkdownPage name="应急处理方法.md" list="must" />}
+            />
             <Route path="/weekly" component={LatestWeekly} />
             <Route path="/monthly" component={LatestMonthly} />
             <Route path="/read" component={ReadList} />
-            <Route path="/weekly_edit" children={<EditPage list="weekly" date={query.get("date")??""}/>} />
-            <Route path="/weekly_work_edit" children={<EditPage list="weekly_work" date={query.get("date")??""}/>} />
-            <Route path="/weekly_view" children={<ShowPage list="weekly"/>} />
-            <Route path="/weekly_work_view" children={<ShowPage list="weekly_work"/>} />
-            <Route path="/friends_edit" children={<EditPage list="friends" date={query.get("date")??""}/>} />
-            <Route path="/friends_view" children={<ShowPage list="friends"/>} />
-            <Route path="/monthly_edit" children={<EditPage list="monthly" date={query.get("date")??""}/>} />
-            <Route path="/monthly_view" children={<ShowPage list="monthly"/>} />
-            <Route path="/principe" component={<MarkdownPage name="原则.md" list="must"/>} />
-            <Route path="/empthy" component={<MarkdownPage name="同理心.md" list="must"/>} />
-        </div>
-      </Content>
-   
-    </Layout>
-  </Layout>
+            {sub.map((item) => {
+              const key = `${item.json}_edit`;
+              return (
+                <Route
+                  path={key}
+                  children={
+                    <EditPage list={item.json} date={query.get("date") ?? ""} />
+                  }
+                />
+              );
+            })}
+            {sub.map((item) => {
+              const key = `${item.json}_view`;
+              return (
+                <Route path={key} children={<ShowPage list={item.json} />} />
+              );
+            })}
 
+            <Route
+              path="/friends_edit"
+              children={
+                <EditPage list="friends" date={query.get("date") ?? ""} />
+              }
+            />
+            <Route
+              path="/principe"
+              children={<MarkdownPage name="原则.md" list="must" />}
+            />
+            <Route
+              path="/empthy"
+              children={<MarkdownPage name="同理心.md" list="must" />}
+            />
+          </div>
+        </Content>
+      </Layout>
+    </Layout>
   );
 }
 
