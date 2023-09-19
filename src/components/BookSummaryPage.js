cf02dd6afec38ed2335e888fde2b92d4fb28c74c
tc <tczhong24@gmail.com>
2023-09-19 00:25:15 -0700

    add highlight

--- src/components/BookSummaryPage.js
+++ src/components/BookSummaryPage.js
@@ -1,5 +1,5 @@
 import Axios from "axios";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import ReactMarkdown from "react-markdown";
 import { dest_url } from "./const";
 import "./BookSummaryPage.css";
@@ -7,15 +7,23 @@ export default function BookSummaryPage(props) {
   const [c] = useState(0);
 
   const [jsonData, setJsonData] = useState([]);
+  const [highlightJsonData, setHighlightJsonData] = useState([]);
   const [selectedSummaryIndex, setSelectedSummaryIndex] = useState(null);
   useEffect(() => {
     Axios.get(
       `https://${dest_url}/json?name=${props.name}&list=${props.list}`
     ).then((data) => {
-      console.log(data);
       setJsonData(data.data.data[0].data);
     });
   }, [c]);
+  useEffect(() => {
+    Axios.get(
+      `https://${dest_url}/json?name=${props.name}&list=${props.list}_highlight`
+    ).then((data) => {
+      setHighlightJsonData(data.data.data[0].data.data);
+    });
+  }, [c]);
+  console.log(highlightJsonData);
   const toggleOriginalText = (index) => {
     if (selectedSummaryIndex === index) {
       setSelectedSummaryIndex(null); // 如果原文已经展示，就隐藏
@@ -23,8 +31,99 @@ export default function BookSummaryPage(props) {
       setSelectedSummaryIndex(index); // 否则就展示
     }
   };
+  const divRef = useRef(null);
+
+  const [toolbarPosition, setToolbarPosition] = useState({ top: 0, left: 0 });
+  const postData = () => {
+    Axios.post(`https://${dest_url}/json?list=${props.list}_highlight`, {
+      date: props.name.split(".")[0], // remove json
+      data: highlightJsonData,
+    }).then(() => {});
+  };
+  const handleHighlight = () => {
+    const selection = window.getSelection();
+    const range = selection.getRangeAt(0);
+    const parentElement = range.commonAncestorContainer.parentElement;
+    const parentparentElement = parentElement.parentElement;
+    const currentIndex = parentparentElement.getAttribute("data-index");
+    const currentType = parentparentElement.getAttribute("class");
+    const newHighlight = {
+      index: parseInt(currentIndex, 10),
+      startOffset: range.startOffset,
+      endOffset: range.endOffset,
+      type: currentType,
+    };
+    const span = document.createElement("span");
+    span.className = "highlight";
+    range.surroundContents(span);
+    selection.removeAllRanges();
+    setHighlightJsonData([...highlightJsonData, newHighlight]);
+  };
+  useEffect(() => {
+    postData();
+  }, [highlightJsonData]);
+
+  const showToolbar = (e) => {
+    const selection = window.getSelection();
+    if (selection.rangeCount > 0) {
+      const range = selection.getRangeAt(0);
+      const rect = range.getBoundingClientRect();
+      setToolbarPosition({
+        top: rect.top - 40,
+        left: rect.left + rect.width / 2,
+      });
+    }
+  };
+
+  useEffect(() => {
+    divRef.current.addEventListener("mouseup", showToolbar);
+    divRef.current.addEventListener("touchend", showToolbar);
+
+    return () => {
+      divRef.current.removeEventListener("mouseup", showToolbar);
+      divRef.current.removeEventListener("touchend", showToolbar);
+    };
+  }, []);
+  const renderHighlightedText = (text, summaryIndex, contentType) => {
+    let lastOffset = 0;
+    const elements = [];
+
+    // 找到这个摘要或原文的所有高亮数据
+    const highlights = highlightJsonData.filter(
+      (highlight) =>
+        highlight.index === summaryIndex && highlight.type === contentType
+    );
+
+    highlights.forEach((highlight) => {
+      // 添加没有高亮的文本
+      elements.push(
+        <span>{text.slice(lastOffset, highlight.startOffset)}</span>
+      );
+      // 添加高亮的文本
+      elements.push(
+        <span className="highlight">
+          {text.slice(highlight.startOffset, highlight.endOffset)}
+        </span>
+      );
+      lastOffset = highlight.endOffset;
+    });
+
+    // 添加剩下的没有高亮的文本
+    elements.push(<span>{text.slice(lastOffset)}</span>);
+
+    return elements;
+  };
   return (
-    <div>
+    <div ref={divRef}>
+      <div
+        style={{
+          position: "absolute",
+          top: `${toolbarPosition.top}px`,
+          left: `${toolbarPosition.left}px`,
+        }}
+      >
+        <button onClick={handleHighlight}>高亮</button>
+      </div>
       {jsonData?.map((book, index) => (
         <div
           className={`book-item ${
@@ -32,15 +131,23 @@ export default function BookSummaryPage(props) {
           }`}
           key={index}
         >
-          <p className="summary">
-            {index + 1}. {book.summary}
+          <p className="summary" data-index={index}>
+            {index + 1}. {renderHighlightedText(book.summary, index, "summary")}
           </p>
           <button onClick={() => toggleOriginalText(index)}>阅读原文</button>
           {selectedSummaryIndex === index && (
-            <p className="original-text">{book.chunk}</p>
+            <p className="original-text" data-index={index}>
+              {renderHighlightedText(book.chunk, index, "original-text")}
+            </p>
           )}
         </div>
       ))}
+
+      <style jsx>{`
+        .highlight {
+          background-color: yellow;
+        }
+      `}</style>
     </div>
   );
 }
